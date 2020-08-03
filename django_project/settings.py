3efcfe919f58bb05089584c13fd12f4799980849
osori <hyeseong43@gmail.com>
2020-08-03 22:56:58 +0900

    Updated ALLOWED HOSTS 2ND

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['osorimagudjangoapp.herokuapp.com/']
+ALLOWED_HOSTS = ['osoridjangoapp.herokuapp.com']
 
 
 # Application definition
