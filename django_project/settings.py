837b91f98af0e682325dc638ebd67d4ac7a9ddb0
osori <hyeseong43@gmail.com>
2020-08-04 13:32:52 +0900

    Added Signature Version

--- django_project/settings.py
+++ django_project/settings.py
@@ -148,5 +148,6 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_SIGNATURE_VERSION =  's3v4'
 
 django_heroku.settings(locals())
